a1c76f51af5976c8188d8346bd37c7e7ee5a6ced
Artem <artemzhdanov1299@gmail.com>
2024-01-06 01:24:54 +0200

    //

--- MVVM-HM/ColorConverter.cs
+++ MVVM-HM/ColorConverter.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace MVVM_HM
 {
-    internal class ColorConverter
+    public class ColorConverter : IMultiValueConverter
     {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 4 && values[0] is byte alpha && values[1] is byte red && values[2] is byte green && values[3] is byte blue)
+            {
+                return new SolidColorBrush(Color.FromArgb(alpha, red, green, blue));
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MVVM-HM/ColorViewModel.cs
+++ MVVM-HM/ColorViewModel.cs
@@ -1,34 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace MVVM_HM
 {
-    public class ColorModel
+    internal class ColorViewModel : ViewModelBase
     {
+        public ObservableCollection<Color> ColorItem { get; set; } = new ObservableCollection<Color>();
 
-        public double _alpha { get; set; }
-        public int _red { get; set; }
-        public int _green { get; set; }
-        public int _blue { get; set; }
+        private static readonly DependencyProperty AlphaProperty = DependencyProperty.Register("Alpha", typeof(double), typeof(ColorViewModel), new PropertyMetadata(0.0, OnColorPropertyChanged));
+        private static readonly DependencyProperty RedProperty = DependencyProperty.Register("Red", typeof(int), typeof(ColorViewModel), new PropertyMetadata(0, OnColorPropertyChanged));
+        private static readonly DependencyProperty GreenProperty = DependencyProperty.Register("Green", typeof(int), typeof(ColorViewModel), new PropertyMetadata(0, OnColorPropertyChanged));
+        private static readonly DependencyProperty BlueProperty = DependencyProperty.Register("Blue", typeof(int), typeof(ColorViewModel), new PropertyMetadata(0, OnColorPropertyChanged));
+        public static readonly DependencyProperty RectangleColorProperty = DependencyProperty.Register("RectangleColor", typeof(Color), typeof(ColorViewModel), new PropertyMetadata(Colors.Black));
 
-        public ColorModel(double alpha, int red, int green, int blue)
+        public double Alpha
         {
-            _alpha = alpha;
-            _red = red;
-            _green = green;
-            _blue = blue;
-            Color.FromArgb((byte)_alpha, (byte)_red, (byte)_green, (byte)_blue);
+            get { return (double)GetValue(AlphaProperty); }
+            set { SetValue(AlphaProperty, value); OnPropertyChanged(nameof(Alpha)); }
         }
 
-        public Color Color
+        public int Red
         {
-            get { return Color.FromArgb((byte)_alpha, (byte)_red, (byte)_green, (byte)_blue); }
+            get { return (int)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); OnPropertyChanged(nameof(Red)); }
         }
+
+        public int Green
+        {
+            get { return (int)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); OnPropertyChanged(nameof(Green)); }
+        }
+
+        public int Blue
+        {
+            get { return (int)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); OnPropertyChanged(nameof(Blue)); }
+        }
+
+        public Color RectangleColor
+        {
+            get { return (Color)GetValue(RectangleColorProperty); }
+            set { SetValue(RectangleColorProperty, value); OnPropertyChanged(nameof(RectangleColor)); }
+        }
+
+
+        private static void OnColorPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = (ColorViewModel)d;
+            viewModel.UpdateRectangleColor();
+        }
+        private void UpdateRectangleColor()
+        {
+            RectangleColor = Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue);
+        }
+
+
+        RelayCommand _addCommand;
+        public ICommand AddCommand
+        {
+            get
+            {
+                if (_addCommand == null)
+                {
+                    _addCommand = new RelayCommand(AddColor, CanAddColor);
+                }
+                return _addCommand;
+            }
+        }
+        private void AddColor(object obj)
+        {
+            ColorItem.Add(Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue));
+        }
+        private bool CanAddColor(object obj)
+        {
+            return true;
+        }
+
 
     }
 }
