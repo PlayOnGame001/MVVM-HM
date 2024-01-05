3b046a764fb79cb996932a23e7649833b3bb9c26
Artem <artemzhdanov1299@gmail.com>
2024-01-06 01:22:48 +0200

    /

---
+++ MVVM-HM/ColorViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MVVM_HM
+{
+    public class ColorModel
+    {
+
+        public double _alpha { get; set; }
+        public int _red { get; set; }
+        public int _green { get; set; }
+        public int _blue { get; set; }
+
+        public ColorModel(double alpha, int red, int green, int blue)
+        {
+            _alpha = alpha;
+            _red = red;
+            _green = green;
+            _blue = blue;
+            Color.FromArgb((byte)_alpha, (byte)_red, (byte)_green, (byte)_blue);
+        }
+
+        public Color Color
+        {
+            get { return Color.FromArgb((byte)_alpha, (byte)_red, (byte)_green, (byte)_blue); }
+        }
+
+    }
+}
